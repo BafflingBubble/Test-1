8a6ce5527bd269f30d05081e856a7f1b084cf9a8
A-Hurley <ahurley1004@bhncdsb.ca>
2021-09-24 10:06:44 -0400

    Update Python_Colours.py
    Green Purple white and black

--- Python_Colours/Python_Colours.py
+++ Python_Colours/Python_Colours.py
@@ -1,4 +1,17 @@
-#hel[p
-#kjfahlkjdsahflakh
-#akjfhksja
-#ajkhfskjad
+
+
+colour = input()
+
+if colour == 'green':
+    print('Green is the color of fresh grass')
+
+elif colour == 'purple':
+    print('Purple is the colour of a grape')
+
+elif colour == 'white':
+    print('The color of purity and innocence white is a true balance of all colors')
+
+elif colour == 'black':
+    print("Black is the colour of a blood sucking crow, and it is a color most people don't know")
+else:
+    print("What you typed in wasn't once of the avaible options")
