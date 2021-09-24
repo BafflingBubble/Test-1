1546e02bc0a931ab8618d69a2a3c9a0ba41c3a0f
MattySanch <msanchuk7@gmail.com>
2021-09-24 10:06:26 -0400

    Update Python_Colours.py

--- Python_Colours/Python_Colours.py
+++ Python_Colours/Python_Colours.py
@@ -10,4 +10,16 @@
 #jshfdsj
 
 #fuck you nick
+colour = input()
 
+if colour == 'pink':
+    print("gross pink is for girls, what are you a woman? more like light-ish red")
+
+elif colour == 'magenta':
+    print("whats magenta? just call it purple-ish pink you idiot")
+
+elif colour == 'blue':
+    print("what are you a sheep? pick a better colour")
+
+elif colour == 'orange':
+    print("Oranges do taste pretty good")
