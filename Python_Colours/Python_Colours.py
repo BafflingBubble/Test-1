2e7b449708204d0001574bcbc20942db39d026b5
A-Hurley <ahurley1004@bhncdsb.ca>
2021-09-24 10:30:19 -0400

    Update Python_Colours.py

--- Python_Colours/Python_Colours.py
+++ Python_Colours/Python_Colours.py
@@ -1,45 +1,48 @@
+var = 1
+while var == 1:
+    print('________________________________________________________________________________________________________')
+    print('Welcome to the colour chooser')
+    print('Please type in the colour you would like from the list of options below')
+    print('Ensure that its in all lowercase and that everything is spelled correctly')
+    print('The options are yellow, red, brown, grey, green, pink, magenta, blue, orange, pruple, white, and black')
+    print("________________________________________________________________________________________________________")
+    colour = input()
 
-colour = input()
+    if colour == "yellow":
+        print("Haha pee colour")
 
-if colour == "yellow":
-    print("Haha pee colour")
+    elif colour == "red":
+        print("Based colour")
 
-elif colour == "red":
-    print("Based colour")
+    elif colour == "brown":
+     print("Eww poo colour")
 
-elif colour == "brown":
-    print("Eww poo colour")
+    elif colour == "grey":
+     print("Stupid colour")
 
-elif colour == "grey":
-    print("Stupid colour")
+    elif colour == 'green':
+        print('Green is the color of fresh grass')
 
-<<<<<<< HEAD
-if colour == 'green':
-=======
-elif colour == 'green':
->>>>>>> 11ee8b90ebaeb2320890710a1bd385ed2ddf797b
-    print('Green is the color of fresh grass')
+    elif colour == 'pink':
+        print("gross pink is for girls, what are you a woman? more like light-ish red")
 
-elif colour == 'pink':
-    print("gross pink is for girls, what are you a woman? more like light-ish red")
+    elif colour == 'magenta':
+        print("whats magenta? just call it purple-ish pink you idiot")
 
-elif colour == 'magenta':
-    print("whats magenta? just call it purple-ish pink you idiot")
+    elif colour == 'blue':
+        print("what are you a sheep? pick a better colour")
 
-elif colour == 'blue':
-    print("what are you a sheep? pick a better colour")
+    elif colour == 'orange':
+        print("Oranges do taste pretty good")
 
-elif colour == 'orange':
-    print("Oranges do taste pretty good")
+    elif colour == 'purple':
+        print('Purple is the colour of a grape')
 
-elif colour == 'purple':
-    print('Purple is the colour of a grape')
+    elif colour == 'white':
+        print('The color of purity and innocence white is a true balance of all colors')
 
-elif colour == 'white':
-    print('The color of purity and innocence white is a true balance of all colors')
-
-elif colour == 'black':
-    print("Black is the colour of a blood sucking crow, and it is a color most people don't know")
+    elif colour == 'black':
+        print("Black is the colour of a blood sucking crow, and it is a color most people don't know")
     
-else:
-    print("What you typed in wasn't once of the avaible options")
+    else:
+        print("What you typed in wasn't once of the avaible options, please try again")
